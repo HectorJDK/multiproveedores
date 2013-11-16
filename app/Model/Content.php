8dd4dac9d09c08a6bb35de2f5facb555b4ee2d35
Hector <hector.hdp90@gmail.com>
2013-11-16 12:45:14 -0600

    Cambios en la creacion de producto

--- app/Model/Content.php
+++ app/Model/Content.php
@@ -65,4 +65,50 @@ class Content extends AppModel {
 		)
 	);
 
+	//Funciones extras de guardado
+	public function new_xml($xml)
+	{
+		$xmlDoc = new DOMDocument('1.0');
+		$xmlDoc->formatOutput = true;
+
+		//Elemento raiz
+		$root = $xmlDoc->createElement('Request');
+		$root = $xmlDoc->appendChild($root);
+
+		//Elemento cliente
+		$customer = $xmlDoc->createElement('Customer');
+		$customer = $root->appendChild($customer);
+
+		//Elemento producto
+		$product = $xmlDoc->createElement('Product');
+		$product = $root->appendChild($product);
+
+
+		//Iterar y agregar los campos
+		foreach ($xml as $campos => $value) {
+			$tipo = substr($campos, 0, 2);
+			$campo = substr($campos, 3);
+			switch($tipo){
+
+				//Campo de cliente
+				case "cl":
+				$campo = $xmlDoc->createElement($campo);
+				$campo = $customer->appendChild($campo);
+				$valor = $xmlDoc->createTextNode($value);
+				$valor = $campo->appendChild($valor);
+				break;
+
+				//Campo de producto
+				case "pd":
+				$campo = $xmlDoc->createElement($campo);
+				$campo = $product->appendChild($campo);
+				$valor = $xmlDoc->createTextNode($value);
+				$valor = $campo->appendChild($valor);
+				break;
+			}
+		}
+		$xml =  $xmlDoc->saveXML();
+		return $xml;
+	}
+
 }
